e41ad692a41ec82bf20ae6f6c5dbecfbf1dfb8fa
SoSorryTT <markpanya@gmail.com>
2021-09-22 19:57:58 +0700

    Add lab05

--- Lab1/lab04.cpp
+++ Lab1/lab04.cpp
@@ -4,28 +4,20 @@ using namespace std;
 
 bool check_prime(int p)
 {
-    if (p <= 1) {
-        return false;
-    }
-    else {
-        int i;
-        for (i=2; i<=p/2; ++i) {
-            if (p % i == 0) {
-                return false;
-            }
-        }
-    }
+  if (p <= 1) {
+    return false;
+  }
+  
 }
 
 int main()
-
 {
-    int p;
-    cin >> p;
+  int p;
+  cin >> p;
 
-    if (check_prime(p)) {
-        cout << "yes" << endl;
-    } else {
-        cout << "no" << endl;
-    }
+  if (check_prime(p)) {
+    cout << "yes" << endl;
+  } else {
+    cout << "no" << endl;
+  }
 }
